396497702f95cc5fa03818ba0d592804b338bf83
Mns65 <minasseh@gmail.com>
2022-04-08 11:15:59 -0700

    Size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Prints to std output
+ *
+ * Return: returns 0.
+ */
+int main(void)
+{
+  printf("Size of a char: %c byte(s), SIZEOF(char)");
+  printf("Size of an int: %d byte(s), SIZEOF(int)");
+  printf("Size of a long int: %d byte(s), SIZEOF(long int)");
+  printf("Size of a long long int: %d byte(s), SIZEOF(long long int)");
+  printf("Size of a float: %f byte(s), SIZEOF(float)");
+  return (0);
+}
