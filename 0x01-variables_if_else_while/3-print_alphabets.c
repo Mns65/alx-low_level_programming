814115d4c0035bc7234fd731be48ec3078d5284f
Mns65 <minasseh@gmail.com>
2022-04-09 07:08:51 -0700

    Both alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabets with small and capital
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
