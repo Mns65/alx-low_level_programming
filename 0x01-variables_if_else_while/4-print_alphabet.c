a2d114cd25024d6e0fc36c2dcab3a376eaf1449b
Mns65 <minasseh@gmail.com>
2022-04-09 08:34:28 -0700

    Alphabet without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - The alphabet without q and e
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c != 'q' && c != 'e'; c <= 'z';, c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
