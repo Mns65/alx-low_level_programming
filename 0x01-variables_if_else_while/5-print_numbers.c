6935aee19728a155a6018f8ef6004aa1be1dbf9f
Mns65 <minasseh@gmail.com>
2022-04-09 23:03:42 -0700

    The numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints the roman numbers
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf(n);
+	}
+	return (0);
+}
