edeb3b6a5beee160aaf3095d60b95cd38cf0e13f
Mns65 <minasseh@gmail.com>
2022-04-12 13:52:35 -0700

    Is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,14 @@
 
 /**
  * _islower - a function that checks if a character is lowercase
- *
+ *@c: character
  * Return: returns 1 if TRUE or 0 if NOT
  */
 
 int _islower(int c)
 {
+	char c;
+	
 	if (_islower(c))
 	{
 		return (1);
