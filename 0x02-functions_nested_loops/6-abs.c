9165b1b8251106f2f9660a445d52184ea3609ed4
Mns65 <minasseh@gmail.com>
2022-04-12 15:08:36 -0700

    Absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,11 @@
 
 /**
  * _abs - Prints absolute values of a number
- *
+ *@n: The number
  * Returns: absolute value
  */
 
-int _abs(int)
+int _abs(int n)
 {
 	int n;
 
@@ -14,4 +14,5 @@ int _abs(int)
 	{
 		n = (-1) * n;
 	}
+	return (n);
 }
