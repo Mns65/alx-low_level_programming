92a7dee18be4248b9af6d9e871863ccab1a32960
Mns65 <minasseh@gmail.com>
2022-04-12 12:48:23 -0700

    The alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in small letters
+ *
+ * Return: the value
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	return (0);
+}
