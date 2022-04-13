906602504563aca82fd96bde13d21a7898bd4013
Mns65 <minasseh@gmail.com>
2022-04-13 09:06:19 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Tests function if it prints integer is positive
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
