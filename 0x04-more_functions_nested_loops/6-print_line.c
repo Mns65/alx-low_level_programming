544f00775ed314b4838bb62cc0bb9f0cea036bfe
Mns65 <minasseh@gmail.com>
2022-04-14 15:59:49 -0700

    Lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ *@n: number of line
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i > 0; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+
+	if (n <= 0)
+		_putchar('\n);
+}
