323f084dca0f7764168b0a0a474220274ac59fe8
Mns65 <minasseh@gmail.com>
2022-04-14 07:49:55 -0700

    Everything

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Tells if a letter is UPPER
+ * Return: 1 if c is upper
+ * Returns 0 otherwise
+ */
+
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
