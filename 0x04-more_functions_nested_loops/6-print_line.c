0acff6c3258c6b0e44a647eff258ede09530aff2
Mns65 <minasseh@gmail.com>
2022-04-15 04:17:33 -0700

    Lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,17 +8,16 @@
 void print_line(int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		_putchar('_');
 
-	}
-
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
 
 	_putchar('\n');
+	}
 }
