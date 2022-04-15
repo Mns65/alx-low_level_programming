6a6e274db80553b3d9c8c74fb799f3ea0be56ea3
Mns65 <minasseh@gmail.com>
2022-04-15 06:53:15 -0700

    I feel like I am diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line
+ *@n: number of diagonals
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(92);
+	}
+
+	_putchar('\n');
+}
