8f3f47d35384fa46e98f248e61d929ed2764bbc0
Mns65 <minasseh@gmail.com>
2022-04-18 08:53:25 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the value of a with b.
+ *@a: The 1st argument
+ *@b: The 2nd argument
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	temp = *b;
+}
