acc7d237f3edee6dfea1a2aa8db2d71752178b97
Mns65 <minasseh@gmail.com>
2022-04-20 06:15:08 -0700

    String copy

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * puts2 - Puts a number character.
+ *@str: The number.
+ *
+ */
+
+void puts2(char *str)
+{
+	
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - Copies a string to another
+ *@dest: the first string
+ *@src: the second string
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while ((dest[i] = src[i]) != '\0')
+	{
+		i++;
+	}
+	return (dest[i] - src[i]);
+
+}
