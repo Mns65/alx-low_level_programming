a5181067f28d5f9556484d75d3344be363f9b1e8
Mns65 <minasseh@gmail.com>
2022-04-21 13:59:04 -0700

    correction

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 
 /**
  * strcat - Concatenates a string
- * @dest: destination string
- * @src: source string
+ *@dest: destination string
+ *@src: source string
  *
  * Return: Returns the destination
  */
@@ -13,10 +13,14 @@ char *_strcat(char *dest, char *src)
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
+	{
 		dest_len++;
+	}
 
 	for (index = 0; src[index]; index++)
+	{
 		dest[dest_len++] = src[index];
+	}
 
 	return (dest);
 }
