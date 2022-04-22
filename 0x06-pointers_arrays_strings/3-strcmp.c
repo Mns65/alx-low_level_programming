2803e6cd79bd39a57cb9c6d6b902c4837e204a49
Mns65 <minasseh@gmail.com>
2022-04-22 12:42:28 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,13 +11,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	
-	while ((s1[i] != '\0') && (s1[i] == s2[i]))
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (s1[i] = '\0')
-			return (0);
+		s1++;
+		s2++;
 	}
 
-	return (s1[i] - s2[i]);
+	return (*s1 - *s2);
 }
