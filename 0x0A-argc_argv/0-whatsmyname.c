f9033a6b9050dfb73295dbe24208b2b938456f79
Mns65 <minasseh@gmail.com>
2022-04-29 12:39:25 -0700

    Everything

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints the number and names of arguments
+ *@argc: the count
+ *@argv: the vector
+ *
+ * Return: Always 0.
+ */
+
+int main (int argc, char *argv[])
+{
+	int argc__attribute__((unused));
+
+	putchar(argv[0]);
+	return (0);
+}
