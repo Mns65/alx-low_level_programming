bd9f5a1525aad502da840ab2bed9e1fcd6800854
Mns65 <minasseh@gmail.com>
2022-05-03 12:10:06 -0700

    Fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,10 +10,18 @@
 
 char *create_array(unsigned int size, char c)
 {
-	int i;
+	unsigned int i;
+	char *array;
 
 	if (size == 0)
 		return (NULL);
+	array = malloc(size * sizeof(char));
+
+	if (array == NULL)
+		return (NULL);
+	
 	for (i = 0; i < size; i++)
-		return (c[i]);
+		array[i] = c;
+
+	return (array);
 }
