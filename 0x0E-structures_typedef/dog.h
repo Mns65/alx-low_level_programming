3e68af7607244fc2c6971864e2a2b3af476b314b
Mns65 <minasseh@gmail.com>
2022-05-09 14:11:00 -0700

    Everything

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Talks about poppy.
+ *@name: The name of the dog
+ *@age: The age 
+ *@owner: The owner.
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
